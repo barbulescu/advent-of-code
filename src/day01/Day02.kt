220e33ce172f02a51d3dc78ebc95e6b7c49595ea
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-02 08:06:32 +0100

    day2 - part1

---
+++ src/day01/Day02.kt
@@ -0,0 +1,68 @@
+package day01
+
+import expectResult
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    expectResult(8) {
+        part1(readInput("Day02_1_test"))
+    }
+//    expectResult(281) {
+//        part2(readInput("Day02_2_test"))
+//    }
+
+    val data = readInput("Day02")
+    println("#1 -> ${part1(data)}")
+//    println("#2 -> ${part2(data)}")
+}
+
+const val MAX_RED = 12
+const val MAX_GREEN = 13
+const val MAX_BLUE = 14
+
+val mainGroupsRegex = Regex("[:;]")
+val setRegex = Regex("\\s*(\\d+)\\s+(\\w+)")
+
+private fun part1(input: List<String>): Int {
+    return input.asSequence()
+        .map(String::parseLine)
+        .filter(Game::isValid)
+        .map(Game::id)
+        .sum()
+}
+
+private fun part2(input: List<String>): Int {
+    return 2
+}
+
+private fun String.parseLine(): Game {
+    val groups = this.split(mainGroupsRegex)
+    val gameId = groups[0].drop(5).toInt()
+    val gameSets = groups.asSequence()
+        .drop(1)
+        .map(String::trim)
+        .map(String::parseGameSet)
+        .toSet()
+
+    return Game(gameId, gameSets)
+}
+
+fun String.parseGameSet(): GameSet {
+    val values = setRegex.findAll(this)
+        .map { it.groupValues }
+        .associate { it[2] to it[1].toInt() }
+    return GameSet(
+        values["red"] ?: 0,
+        values["green"] ?: 0,
+        values["blue"] ?: 0,
+    )
+}
+
+data class Game(val id: Int, val sets: Set<GameSet>) {
+    fun isValid() = sets.all { it.isValid() }
+}
+
+data class GameSet(val red: Int, val green: Int, val blue: Int) {
+    fun isValid() = red <= MAX_RED && green <= MAX_GREEN && blue <= MAX_BLUE
+}
