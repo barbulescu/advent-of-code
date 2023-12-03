d2969c9b4241c3e88ad18ea06307a888a899c0f8
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-03 14:53:16 +0100

    prepare day 4

---
+++ src/Day04.kt
@@ -0,0 +1,26 @@
+import utils.expectResult
+import utils.readInput
+
+private const val DAY = "Day04"
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    expectResult(1) {
+        part1(readInput("${DAY}_1_test"))
+    }
+//    expectResult(1) {
+//        part2(readInput("${DAY}_2_test"))
+//    }
+
+//    val data = readInput(DAY)
+//    println("#1 -> ${part1(data)}")
+//    println("#2 -> ${part2(data)}")
+}
+
+private fun part1(input: List<String>): Int {
+    return -1
+}
+
+private fun part2(input: List<String>): Int {
+    return -1
+}
