78f0b513c5f55ccc397389e3f1847d2a8bf10df4
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-06 09:16:56 +0100

    day 6 part 2

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,18 +1,19 @@
 package day06
 
 import utils.FileData
+import utils.expectLongResult
 import utils.expectResult
 
 private val fileData = FileData(6)
 
 fun main() {
     // test if implementation meets criteria from the description, like:
-    expectResult(288) {
+    expectLongResult(288) {
         part1(fileData.readTestData(1))
     }
-//    expectResult(1) {
-//        part2(fileData.readTestData(2))
-//    }
+    expectLongResult(71503) {
+        part2(fileData.readTestData(2))
+    }
 
     val data = fileData.readData()
 
@@ -20,12 +21,17 @@ fun main() {
     println("#2 -> ${part2(data)}")
 }
 
-private fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Long {
     return input.parseRaceRecords().countAllWinCases()
 }
 
-private fun part2(input: List<String>): Int {
-    return -1
+private fun part2(input: List<String>): Long {
+    require(input.size == 2) { "expect 2 lines but got got $input" }
+    val race = Race(
+        time = String(input[0].drop(5).toCharArray().filterNot { it == ' ' }.toCharArray()).toLong(),
+         distance = String(input[1].drop(10).toCharArray().filterNot { it == ' ' }.toCharArray()).toLong()
+    )
+    return race.validWinCases()
 }
 
 val separatorRegex = Regex(" +")
@@ -44,20 +50,21 @@ fun List<String>.parseRaceRecords(): RaceRecords {
     require(times.size == distances.size) { "times and distances count must be the same" }
 
     val raceRecords = times.zip(distances)
-        .map { Race(it.first, it.second) }
+        .map { Race(it.first.toLong(), it.second.toLong()) }
     return RaceRecords(raceRecords)
 }
 
 data class RaceRecords(val races: List<Race>) {
-    fun countAllWinCases() = races.map { it.validWinCases() }.fold(1) {acc, i -> acc * i }
+    fun countAllWinCases() = races.map { it.validWinCases() }.fold(1L) {acc, i -> acc * i }
 }
 
-data class Race(val time: Int, val distance: Int) {
-    fun canBeatRecord(pressTime: Int) = (time - pressTime) * pressTime > distance
+data class Race(val time: Long, val distance: Long) {
+    fun canBeatRecord(pressTime: Long) = (time - pressTime) * pressTime > distance
 
-    fun validWinCases() : Int = (0..time)
+    fun validWinCases() : Long = (0..time)
         .asSequence()
         .filter { canBeatRecord(it) }
         .count()
+        .toLong()
 }
 
