d4b64de960c3246a58f3ed4d2e9601e62522d302
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-07 21:37:48 +0100

    prepare day 8

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,29 @@
+package day08
+
+import utils.FileData
+import utils.expectResult
+
+private val fileData = FileData(8)
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    expectResult(1) {
+        part1(fileData.readTestData(1))
+    }
+    expectResult(1) {
+        part2(fileData.readTestData(2))
+    }
+
+    val data = fileData.readData()
+
+    println("#1 -> ${part1(data)}")
+    println("#2 -> ${part2(data)}")
+}
+
+private fun part1(input: List<String>): Int {
+    return -1
+}
+
+private fun part2(input: List<String>): Int {
+    return -1
+}
