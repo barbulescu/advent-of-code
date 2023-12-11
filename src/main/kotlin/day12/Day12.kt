a756ee5255a941f8410170a40c2fa0147cfcfe6c
Marius Barbulescu <marius.barbulescu@gmail.com>
2023-12-11 21:20:13 +0100

    prepare day 12

---
+++ src/main/kotlin/day12/Day12.kt
@@ -0,0 +1,29 @@
+package day12
+
+import utils.FileData
+import utils.expectResult
+
+private val fileData = FileData(12)
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    expectResult(1) {
+        part1(fileData.readTestData(1))
+    }
+    expectResult(1) {
+        part2(fileData.readTestData(2))
+    }
+
+    val data = fileData.readData()
+
+    println("#1 -> ${part1(data)}")
+    println("#2 -> ${part2(data)}")
+}
+
+private fun part1(lines: List<String>): Int {
+    return -1
+}
+
+private fun part2(lines: List<String>): Int {
+    return -1
+}
