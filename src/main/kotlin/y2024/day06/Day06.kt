4f5f62d085f68ceb49c49e23ab3bc519ef4e7e78
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-06 22:17:39 +0100

    extract findGuard 2024.6

--- src/main/kotlin/y2024/day06/Day06.kt
+++ src/main/kotlin/y2024/day06/Day06.kt
@@ -4,7 +4,11 @@ import utils.executeDay
 import y2024.day06.Orientation.*
 import java.util.*
 import kotlin.streams.asSequence
-import kotlin.streams.asStream
+
+
+fun main() {
+    executeDay(List<String>::part1, List<String>::part2)
+}
 
 enum class Orientation {
     NORTH,
@@ -30,49 +34,35 @@ data class Guard(val x: Int, val y: Int, val orientation: Orientation) {
 }
 
 class Lab(private val data: MutableList<MutableList<Char>>) {
-    val initialPosition = findGuard()
     private val cols = data[0].size
     private val rows = data.size
 
-    fun markVisited(guard: Guard) {
+    fun markAsVisited(guard: Guard) {
         data[guard.y][guard.x] = 'X'
     }
 
     fun isBlocked(guard: Guard): Boolean = data[guard.y][guard.x] == '#'
 
-    fun isNotLeaving(guard: Guard): Boolean = when (guard.orientation) {
+    fun isNextMoveInside(guard: Guard): Boolean = when (guard.orientation) {
         NORTH -> guard.y != 0
         EAST -> guard.x != cols - 1
         SOUTH -> guard.y != rows - 1
         WEST -> guard.x != 0
     }
 
-    fun visited() = data.flatten()
+    fun countVisitedPositions() = data.flatten()
         .count { it == 'X' }
         .toLong()
-
-    private fun findGuard(): Guard {
-        data.forEachIndexed { y, line ->
-            line.forEachIndexed { x, c ->
-                if (c == '^') {
-                    return Guard(x, y, NORTH)
-                }
-            }
-        }
-        error("Unable to find guard")
-    }
-}
-
-fun main() {
-    executeDay(List<String>::part1, List<String>::part2)
 }
 
 private fun List<String>.part1(): Long {
     val lab: Lab = buildLab()
-    var guard = lab.initialPosition
+    val startGuard = findGuard()
 
-    lab.markVisited(guard)
-    while (lab.isNotLeaving(guard)) {
+    var guard = startGuard
+
+    lab.markAsVisited(guard)
+    while (lab.isNextMoveInside(guard)) {
         val nexPosition = guard.move()
 
         guard = if (lab.isBlocked(nexPosition)) {
@@ -80,32 +70,21 @@ private fun List<String>.part1(): Long {
         } else {
             nexPosition
         }
-        lab.markVisited(guard)
+        lab.markAsVisited(guard)
     }
 
-    return lab.visited()
-}
-
-private fun List<String>.buildLab(): Lab = Lab(buildLabMap())
-
-private fun List<String>.buildLabMap(): MutableList<MutableList<Char>> {
-    val map: MutableList<MutableList<Char>> = this.map { line ->
-        line.chars()
-            .asSequence()
-            .map(Int::toChar)
-            .toMutableList()
-    }.toMutableList()
-    return map
+    return lab.countVisitedPositions()
 }
 
 private fun List<String>.part2(): Long = buildLabVariants()
     .count { (lab, _) ->
         var found = false
-        var guard = lab.initialPosition
+        val startGuard = findGuard()
+        var guard = startGuard
 
-        lab.markVisited(guard)
+        lab.markAsVisited(guard)
         var count = 0
-        while (lab.isNotLeaving(guard)) {
+        while (lab.isNextMoveInside(guard)) {
             count++
             if (count > 10000) {
                 found = true
@@ -118,7 +97,7 @@ private fun List<String>.part2(): Long = buildLabVariants()
             } else {
                 nexPosition
             }
-            if (guard == lab.initialPosition) {
+            if (guard == startGuard) {
                 found = true
                 break
             }
@@ -126,6 +105,13 @@ private fun List<String>.part2(): Long = buildLabVariants()
         found
     }
     .toLong()
+
+private fun List<String>.findGuard() = this
+    .asSequence()
+    .mapIndexed { y: Int, line: String -> y to line.indexOf('^') }
+    .filter { it.second >= 0 }
+    .map { Guard(it.second, it.first, NORTH) }
+    .first()
 
 private fun List<String>.buildLabVariants(): Sequence<Pair<Lab, String>> = sequence {
     forEachIndexed { y, line ->
@@ -139,3 +125,14 @@ private fun List<String>.buildLabVariants(): Sequence<Pair<Lab, String>> = seque
     }
 }
 
+private fun List<String>.buildLab(): Lab = Lab(buildLabMap())
+
+private fun List<String>.buildLabMap(): MutableList<MutableList<Char>> {
+    val map: MutableList<MutableList<Char>> = this.map { line ->
+        line.chars()
+            .asSequence()
+            .map(Int::toChar)
+            .toMutableList()
+    }.toMutableList()
+    return map
+}
