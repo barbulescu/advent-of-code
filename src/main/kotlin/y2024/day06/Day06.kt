bca6a5eea0c58dfc5988e3dc08ce03a8f80e3472
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-06 22:32:56 +0100

    extract playGuard 2024.6

--- src/main/kotlin/y2024/day06/Day06.kt
+++ src/main/kotlin/y2024/day06/Day06.kt
@@ -37,13 +37,13 @@ class Lab(private val data: MutableList<MutableList<Char>>) {
     private val cols = data[0].size
     private val rows = data.size
 
-    fun markAsVisited(guard: Guard) {
+    private fun markAsVisited(guard: Guard) {
         data[guard.y][guard.x] = 'X'
     }
 
-    fun isBlocked(guard: Guard): Boolean = data[guard.y][guard.x] == '#'
+    private fun isBlocked(guard: Guard): Boolean = data[guard.y][guard.x] == '#'
 
-    fun isNextMoveInside(guard: Guard): Boolean = when (guard.orientation) {
+    private fun isNextMoveInside(guard: Guard): Boolean = when (guard.orientation) {
         NORTH -> guard.y != 0
         EAST -> guard.x != cols - 1
         SOUTH -> guard.y != rows - 1
@@ -53,58 +53,39 @@ class Lab(private val data: MutableList<MutableList<Char>>) {
     fun countVisitedPositions() = data.flatten()
         .count { it == 'X' }
         .toLong()
-}
 
-private fun List<String>.part1(): Long {
-    val lab: Lab = buildLab()
-    val startGuard = findGuard()
-
-    var guard = startGuard
-
-    lab.markAsVisited(guard)
-    while (lab.isNextMoveInside(guard)) {
-        val nexPosition = guard.move()
-
-        guard = if (lab.isBlocked(nexPosition)) {
-            guard.rotate()
-        } else {
-            nexPosition
-        }
-        lab.markAsVisited(guard)
-    }
-
-    return lab.countVisitedPositions()
-}
-
-private fun List<String>.part2(): Long = buildLabVariants()
-    .count { (lab, _) ->
-        var found = false
-        val startGuard = findGuard()
+    fun playGuard(startGuard: Guard): Boolean {
+        markAsVisited(startGuard)
         var guard = startGuard
-
-        lab.markAsVisited(guard)
         var count = 0
-        while (lab.isNextMoveInside(guard)) {
-            count++
-            if (count > 10000) {
-                found = true
-                break
-            }
+        while (isNextMoveInside(guard)) {
             val nexPosition = guard.move()
 
-            guard = if (lab.isBlocked(nexPosition)) {
+            guard = if (isBlocked(nexPosition)) {
                 guard.rotate()
             } else {
                 nexPosition
             }
-            if (guard == startGuard) {
-                found = true
-                break
+            markAsVisited(guard)
+            count++
+
+            if (guard == startGuard || count > 10_000) {
+                return true
             }
         }
-        found
+        return false
     }
+}
+
+private fun List<String>.part1(): Long = buildLab().run {
+    playGuard(findGuard())
+    countVisitedPositions()
+}
+
+private fun List<String>.part2(): Long = buildLabVariants()
+    .count { (lab, _) -> lab.playGuard(findGuard()) }
     .toLong()
+
 
 private fun List<String>.findGuard() = this
     .asSequence()
@@ -127,12 +108,9 @@ private fun List<String>.buildLabVariants(): Sequence<Pair<Lab, String>> = seque
 
 private fun List<String>.buildLab(): Lab = Lab(buildLabMap())
 
-private fun List<String>.buildLabMap(): MutableList<MutableList<Char>> {
-    val map: MutableList<MutableList<Char>> = this.map { line ->
-        line.chars()
-            .asSequence()
-            .map(Int::toChar)
-            .toMutableList()
-    }.toMutableList()
-    return map
-}
+private fun List<String>.buildLabMap(): MutableList<MutableList<Char>> = map { line ->
+    line.chars()
+        .asSequence()
+        .map(Int::toChar)
+        .toMutableList()
+}.toMutableList()
