5806773cd33f438168d64e79211019a92dd4df8f
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-09 18:12:00 +0100

    add swap functions

--- src/main/kotlin/utils/Extensions.kt
+++ src/main/kotlin/utils/Extensions.kt
@@ -1,3 +1,15 @@
 package utils
 
 fun List<Int>.middle() = this[size / 2]
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+}
+
+fun StringBuilder.swap(index1: Int, index2: Int) {
+    val temp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = temp
+}
