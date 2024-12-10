33528bcd23a79a0766bce02e3790d28c24ea8d63
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-10 05:51:58 +0100

    add all days skeleton

--- src/main/kotlin/y2024/day09/Day09.kt
+++ src/main/kotlin/y2024/day09/Day09.kt
@@ -1,6 +1,8 @@
 package y2024.day09
 
 import utils.executeDay
+import utils.swap
+import java.util.*
 
 fun main() {
     executeDay(List<String>::part1, List<String>::part2)
@@ -12,8 +14,7 @@ private fun List<String>.part1(): Long {
     for (l in initialMap.indices.reversed()) {
         if (initialMap[l] != Char.MAX_VALUE) {
             val i = initialMap.indexOf(Char.MAX_VALUE)
-            initialMap[i] = initialMap[l]
-            initialMap[l] = Char.MAX_VALUE
+            initialMap.swap(i, l)
         }
         val a = initialMap.indexOfLast { it != Char.MAX_VALUE } + 1
         val b = initialMap.indexOf(Char.MAX_VALUE)
@@ -22,11 +23,12 @@ private fun List<String>.part1(): Long {
         }
     }
 
-    return initialMap
-        .takeWhile { it != Char.MAX_VALUE }
-        .mapIndexed { index, c -> index.toLong() * c.code.toLong() }
-        .sum()
+    return initialMap.toChecksum()
 }
+
+private fun StringBuilder.toChecksum() = filterNot { it == Char.MAX_VALUE }
+    .mapIndexed { index, c -> index.toLong() * c.code.toLong() }
+    .sum()
 
 private fun String.toMap(): String = toCharArray()
     .flatMapIndexed { i, c ->
@@ -39,5 +41,74 @@ private fun String.toMap(): String = toCharArray()
     }.joinToString(separator = "")
 
 private fun List<String>.part2(): Long {
-    TODO("part 2 is not yet implemented")
+    val disk = this[0].toCharArray().mapIndexed { i, c ->
+        val id = i / 2
+        val type = if (i % 2 == 0) {
+            Type.DATA
+        } else {
+            Type.FREE
+        }
+        Block(id, type, c.digitToInt())
+    }.toMutableList()
+
+    var dataIndex = disk.size
+    while (dataIndex > 0) {
+        dataIndex--
+
+        val data = disk[dataIndex]
+
+        if (data.type == Type.FREE) {
+            continue
+        }
+        if (data.size == 0) {
+            continue
+        }
+        val freeIndex = disk.indexOfFirst { it.type == Type.FREE && it.size >= data.size }
+        if (freeIndex < 0 || freeIndex >= dataIndex) {
+            continue
+        }
+
+        val free = disk[freeIndex]
+        if (free.size == data.size) {
+            disk.swap(dataIndex, freeIndex)
+            continue
+        }
+        val freeBlock1 = free.copy(size = data.size)
+        val freeBlock2 = free.copy(size = free.size - data.size)
+        disk[freeIndex] = data
+        disk[dataIndex] = freeBlock1
+        disk.add(freeIndex + 1, freeBlock2)
+    }
+
+    var index = 0
+    return disk.sumOf {
+        val response = it.calculate(index)
+        index += it.size
+        response
+    }
+
+}
+
+private data class Block(val id: Int, val type: Type, val size: Int) {
+    fun calculate(startIndex: Int): Long = if (type == Type.FREE) {
+        0
+    } else {
+        (0..<size)
+            .map { it + startIndex }
+            .sumOf { id * it }
+            .toLong()
+    }
+
+    override fun toString(): String =
+        String(
+            if (type == Type.FREE) {
+                List(size) { '.' }
+            } else {
+                List(size) { id.toChar() }
+            }.toCharArray())
+}
+
+private enum class Type {
+    DATA,
+    FREE
 }
---
+++ src/main/kotlin/y2024/day21/Day21.kt
@@ -0,0 +1,15 @@
+package y2024.day10
+
+import utils.executeDay
+
+fun main() {
+    executeDay(List<String>::part1, List<String>::part2)
+}
+
+private fun List<String>.part1(): Long {
+    TODO("part 1 is not yet implemented")
+}
+
+private fun List<String>.part2(): Long {
+    TODO("part 2 is not yet implemented")
+}
