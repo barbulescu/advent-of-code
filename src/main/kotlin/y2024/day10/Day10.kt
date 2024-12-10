65fdd22e7c866c3571622a4669842fa0897daa1a
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-10 12:15:10 +0100

    improve 2024.10

--- src/main/kotlin/y2024/day10/Day10.kt
+++ src/main/kotlin/y2024/day10/Day10.kt
@@ -10,7 +10,7 @@ private fun List<String>.part1(): Long = execute { 0 }
 
 private fun List<String>.part2(): Long = execute { 1 }
 
-private fun List<String>.execute(rating: () -> Int): Long {
+private fun List<String>.execute(ratingProvider: () -> Int): Long {
     val grid = this.mapIndexed { y, line ->
         line.mapIndexed { x, c ->
             Point(x, y, c.digitToIntOrNull() ?: -1)
@@ -19,10 +19,19 @@ private fun List<String>.execute(rating: () -> Int): Long {
 
     return grid
         .flatten()
-        .filter { it.height == 0 }
-        .sumOf { grid.findAllTrails(mutableSetOf(), it, it, rating) }
+        .startingPoints()
+        .sumOf {
+            grid.findAllTrails(
+                paths = mutableSetOf(),
+                start = it,
+                point = it,
+                ratingProvider = ratingProvider
+            )
+        }
         .toLong()
 }
+
+private fun List<Point>.startingPoints(): List<Point> = filter { it.height == 0 }
 
 private data class Point(val x: Int, val y: Int, val height: Int)
 
@@ -41,12 +50,12 @@ private fun List<List<Point>>.findAllTrails(
     paths: MutableSet<Path>,
     start: Point,
     point: Point,
-    rating: () -> Int,
+    ratingProvider: () -> Int,
 ): Int {
     if (point.height == 9) {
         val path = Path(start, point)
         return if (path in paths) {
-            rating()
+            ratingProvider()
         } else {
             paths.add(path)
             1
@@ -56,7 +65,7 @@ private fun List<List<Point>>.findAllTrails(
         .filterNotNull()
         .filter { it.height == point.height + 1 }
         .map {
-            findAllTrails(paths, start, it, rating)
+            findAllTrails(paths, start, it, ratingProvider)
         }
         .sum()
 }
@@ -69,13 +78,9 @@ private fun List<List<Point>>.neighbours(x: Int, y: Int) = sequenceOf(
     pointAt(x + 1, y)
 )
 
-private fun List<List<Point>>.pointAt(x: Int, y: Int): Point? {
-    if (x < 0 || x >= this[0].size) {
-        return null
-    }
-    if (y < 0 || y >= this.size) {
-        return null
-    }
-    return this[y][x]
+private fun List<List<Point>>.pointAt(x: Int, y: Int): Point? = when {
+    x < 0 || x >= this[0].size -> null
+    y < 0 || y >= this.size -> null
+    else -> this[y][x]
 }
 
