f94477b7e33741d0287e45ae44345ff70c8062e5
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-10 07:13:47 +0100

    improve 2024.10

--- src/main/kotlin/y2024/day10/Day10.kt
+++ src/main/kotlin/y2024/day10/Day10.kt
@@ -6,34 +6,25 @@ fun main() {
     executeDay(List<String>::part1, List<String>::part2)
 }
 
-private fun List<String>.part1(): Long {
+private fun List<String>.part1(): Long = execute { 0 }
+
+private fun List<String>.part2(): Long = execute { 1 }
+
+private fun List<String>.execute(rating: () -> Int): Long {
     val grid = this.mapIndexed { y, line ->
-        line.mapIndexed { x, c -> Point(x, y, c.digitToIntOrNull() ?: -1) }
+        line.mapIndexed { x, c ->
+            Point(x, y, c.digitToIntOrNull() ?: -1)
+        }
     }
 
     return grid
         .flatten()
         .filter { it.height == 0 }
-        .sumOf { grid.findAllTrails(mutableSetOf(), it, it) }
-        .toLong()
-
-}
-
-private fun List<String>.part2(): Long {
-    val grid = this.mapIndexed { y, line ->
-        line.mapIndexed { x, c -> Point(x, y, c.digitToIntOrNull() ?: -1) }
-    }
-
-    return grid
-        .flatten()
-        .filter { it.height == 0 }
-        .sumOf { grid.calculateRatings(mutableSetOf(), it, it) }
+        .sumOf { grid.findAllTrails(mutableSetOf(), it, it, rating) }
         .toLong()
 }
 
-private data class Point(val x: Int, val y: Int, val height: Int) {
-    override fun toString(): String = "$x:$y-$height"
-}
+private data class Point(val x: Int, val y: Int, val height: Int)
 
 private data class Path(val start: Point, val end: Point) {
     init {
@@ -46,40 +37,28 @@ private data class Path(val start: Point, val end: Point) {
     }
 }
 
-private fun List<List<Point>>.findAllTrails(paths: MutableSet<Path>, start: Point, point: Point): Int {
+private fun List<List<Point>>.findAllTrails(
+    paths: MutableSet<Path>,
+    start: Point,
+    point: Point,
+    rating: () -> Int,
+): Int {
     if (point.height == 9) {
         val path = Path(start, point)
         return if (path in paths) {
-            0
+            rating()
         } else {
             paths.add(path)
             1
         }
     }
-    val sum = neighbours(point.x, point.y)
+    return neighbours(point.x, point.y)
         .filterNotNull()
         .filter { it.height == point.height + 1 }
-        .map { findAllTrails(paths, start, it) }
-        .sum()
-    return sum
-}
-
-private fun List<List<Point>>.calculateRatings(paths: MutableSet<Path>, start: Point, point: Point): Int {
-    if (point.height == 9) {
-        val path = Path(start, point)
-        return if (path in paths) {
-            1
-        } else {
-            paths.add(path)
-            1
+        .map {
+            findAllTrails(paths, start, it, rating)
         }
-    }
-    val sum = neighbours(point.x, point.y)
-        .filterNotNull()
-        .filter { it.height == point.height + 1 }
-        .map { calculateRatings(paths, start, it) }
         .sum()
-    return sum
 }
 
 
