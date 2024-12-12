546cd65e505d097f9f42c7561eedb188af06c783
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-12 07:33:42 +0100

    refactor 2024.12.1

--- src/main/kotlin/y2024/day12/Day12.kt
+++ src/main/kotlin/y2024/day12/Day12.kt
@@ -6,13 +6,27 @@ fun main() {
     executeDay(List<String>::part1, List<String>::part2)
 }
 
-private fun List<String>.part1(): Long = this.toGrid()
-    .run {
-        plants()
-            .filterNot { it.visited }
-            .forEach { processPlat(it) }
-        price()
-    }
+private fun List<String>.part1(): Long {
+    val grid = this.toGrid()
+    return grid
+        .plants()
+        .filterNot { it.visited }
+        .sumOf { rootPlant ->
+            val plants = grid
+                .processPlat(rootPlant)
+                .toList()
+
+            val perimeter = plants
+                .sumOf { plant ->
+                    grid.neighbours(plant)
+                        .count { it == null || it.type != plant.type }
+                }
+
+            plants.size * perimeter
+        }
+        .toLong()
+
+}
 
 private fun List<String>.part2(): Long {
     TODO("part 2 is not yet implemented")
@@ -26,41 +40,25 @@ private fun List<String>.toGrid(): Grid {
 }
 
 private class Grid(private val data: List<List<Plant>>) {
-    private var price = 0L
-    private val cache = mutableMapOf<PlantType, Plot>()
-
     fun plants() = data.asSequence().flatten()
 
-    fun processPlat(plant: Plant = data[0][0], depth: Int = 0) {
+    fun processPlat(plant: Plant = data[0][0]): Sequence<Plant> {
         if (plant.visited) {
-            return
+            return emptySequence()
         }
-//        println("Processing $plant $depth")
-
-        val plot = cache.getOrPut(plant.type) { Plot(plant.type) }
-
-        val neighbours = neighbours(plant.x, plant.y)
-        val perimeter = neighbours.count { it == null || it.type != plant.type }
-        plot.addPlant(perimeter)
         plant.visited = true
 
-        neighbours
+        return neighbours(plant)
             .filterNotNull()
             .filter { it.type == plant.type }
-            .filterNot { it.visited }
-            .forEach { processPlat(it, depth + 1) }
-
-        if (depth == 0) {
-            price += plot.price()
-            cache.remove(plant.type)
-        }
+            .flatMap { processPlat(it) } + plant
     }
 
-    private fun neighbours(x: Int, y: Int) = listOf(
-        pointAt(x - 1, y),
-        pointAt(x, y - 1),
-        pointAt(x, y + 1),
-        pointAt(x + 1, y),
+    fun neighbours(plant: Plant) = sequenceOf(
+        pointAt(plant.x - 1, plant.y),
+        pointAt(plant.x, plant.y - 1),
+        pointAt(plant.x, plant.y + 1),
+        pointAt(plant.x + 1, plant.y),
     )
 
     private fun pointAt(x: Int, y: Int): Plant? {
@@ -72,28 +70,9 @@ private class Grid(private val data: List<List<Plant>>) {
         }
         return data[y][x]
     }
-
-
-    fun price() = price
 }
 
 private data class Plant(val x: Int, val y: Int, val type: PlantType, var visited: Boolean = false)
-
-private class Plot(val type: PlantType) {
-    private var area: Long = 0
-    private var perimeter: Long = 0
-
-    fun addPlant(perimeter: Int) {
-        this.perimeter += perimeter
-        this.area++
-    }
-
-    fun price(): Long {
-        val response = area * perimeter
-        println("${type.char} -> $area * $perimeter = $response")
-        return response
-    }
-}
 
 @JvmInline
 value class PlantType(val char: Char)
