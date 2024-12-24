4b6236c37cfc3314c550768e25ec38d678b4429d
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-24 19:54:13 +0100

    use Boolean instead of Int

--- src/main/kotlin/y2024/day24/Day24.kt
+++ src/main/kotlin/y2024/day24/Day24.kt
@@ -2,49 +2,56 @@ package y2024.day24
 
 class Day24(private val lines: List<String>) {
     fun part1(): Long {
-        val inputs: Map<String, Int> = lines
+        val inputs: Map<String, Boolean> = lines
             .takeWhile(String::isNotBlank)
             .map { it.split(": ") }
-            .associate { (a, b) -> a to b.toInt() }
+            .associate { (a, b) -> a to (b == "1") }
 
         val gates: MutableMap<String, Any> = lines
             .takeLastWhile(String::isNotBlank)
             .map { it.split(" -> ") }
             .associate { (a, b) -> b to a }
             .mapValues { (_, v) -> v.split(" ") }
-            .mapValues { (_, v) -> Operation(v[0], v[1], v[2]) }
+            .mapValues { (k, v) -> v.toOperation(k) }
             .toMutableMap()
 
         return runCircuit(gates, inputs)
     }
 
+    private fun List<String>.toOperation(c: String): Operation {
+        val eval = when (this[1]) {
+            "AND" -> Boolean::and
+            "OR" -> Boolean::or
+            "XOR" -> Boolean::xor
+            else -> error("Invalid operation: ${this[1]}")
+        }
+
+        return Operation(this[0], this[2], c, eval)
+    }
+
     fun part2(): String = TODO("Not implemented")
 
-    private fun runCircuit(gates: MutableMap<String, Any>, inputs: Map<String, Int>) = gates.keys
+    private fun runCircuit(gates: MutableMap<String, Any>, inputs: Map<String, Boolean>) = gates.keys
         .filter { it.startsWith("z") }
         .sorted()
         .reversed()
         .map { calculate(gates, inputs, it) }
+        .map { if (it) 1 else 0 }
         .joinToString(separator = "") { it.toString() }
         .toLong(2)
 
-    private fun calculate(gates: MutableMap<String, Any>, inputs: Map<String, Int>, output: String): Int {
+    private fun calculate(gates: MutableMap<String, Any>, inputs: Map<String, Boolean>, output: String): Boolean {
         val value = gates[output] ?: inputs.getValue(output)
-        if (value is Int) {
+        if (value is Boolean) {
             return value
         }
         require(value is Operation) { "Invalid value: $value" }
         val a = calculate(gates, inputs, value.a)
         val b = calculate(gates, inputs, value.b)
-        val result = when (value.op) {
-            "AND" -> a and b
-            "OR" -> a or b
-            "XOR" -> a xor b
-            else -> error("Invalid operation: ${value.op}")
-        }
+        val result = value.eval(a, b)
         gates[output] = result
         return result
     }
 
-    data class Operation(val a: String, val op: String, val b: String)
+    data class Operation(val a: String, val b: String, val c: String, val eval: (Boolean, Boolean) -> Boolean)
 }
