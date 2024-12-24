6ced185692b29e13daa193925d683df2b6a22a64
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-12-24 19:46:28 +0100

    pass inputs and gates to runCircuit function

--- src/main/kotlin/y2024/day24/Day24.kt
+++ src/main/kotlin/y2024/day24/Day24.kt
@@ -1,34 +1,41 @@
 package y2024.day24
 
-class Day24(lines: List<String>) {
-    private val inputs: Map<String, Int> = lines
-        .takeWhile { it.isNotBlank() }
-        .map { it.split(": ") }
-        .associate { (a, b) -> a to b.toInt() }
+class Day24(private val lines: List<String>) {
+    fun part1(): Long {
+        val inputs: Map<String, Int> = lines
+            .takeWhile(String::isNotBlank)
+            .map { it.split(": ") }
+            .associate { (a, b) -> a to b.toInt() }
 
-    private val gates: MutableMap<String, Any> = lines.takeLastWhile { it.isNotBlank() }
-        .map { it.split(" -> ") }
-        .associate { (a, b) -> b to a }
-        .mapValues { (_, v) -> v.split(" ") }
-        .mapValues { (_, v) -> Operation(v[0], v[1], v[2]) }
-        .toMutableMap()
+        val gates: MutableMap<String, Any> = lines
+            .takeLastWhile(String::isNotBlank)
+            .map { it.split(" -> ") }
+            .associate { (a, b) -> b to a }
+            .mapValues { (_, v) -> v.split(" ") }
+            .mapValues { (_, v) -> Operation(v[0], v[1], v[2]) }
+            .toMutableMap()
 
-    fun part1(): Long = gates.keys
+        return runCircuit(gates, inputs)
+    }
+
+    fun part2(): String = TODO("Not implemented")
+
+    private fun runCircuit(gates: MutableMap<String, Any>, inputs: Map<String, Int>) = gates.keys
         .filter { it.startsWith("z") }
         .sorted()
         .reversed()
-        .map { calculate(it) }
+        .map { calculate(gates, inputs, it) }
         .joinToString(separator = "") { it.toString() }
         .toLong(2)
 
-    private fun calculate(output: String): Int {
+    private fun calculate(gates: MutableMap<String, Any>, inputs: Map<String, Int>, output: String): Int {
         val value = gates[output] ?: inputs.getValue(output)
         if (value is Int) {
             return value
         }
         require(value is Operation) { "Invalid value: $value" }
-        val a = calculate(value.a)
-        val b = calculate(value.b)
+        val a = calculate(gates, inputs, value.a)
+        val b = calculate(gates, inputs, value.b)
         val result = when (value.op) {
             "AND" -> a and b
             "OR" -> a or b
@@ -38,8 +45,6 @@ class Day24(lines: List<String>) {
         gates[output] = result
         return result
     }
-
-    fun part2(): String = TODO("Not implemented")
 
     data class Operation(val a: String, val op: String, val b: String)
 }
